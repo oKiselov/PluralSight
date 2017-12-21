0f03ec0bab6091f0812d665d98a9aa3b3e6ff46f
Alex Kiselov <alex.kiselov@gilliesoftware.com>
2017-12-21 18:27:45 +0200

    First educational Akka.Net project was added

---
+++ Projects/MovieStreaming/MovieStreaming/Program.cs
@@ -0,0 +1,17 @@
+﻿using Akka.Actor;
+using Akka.Actor.Internal;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+
+        }
+    }
+}
